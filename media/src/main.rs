48e413029eb8e5a292c5c26a19d884a2e46ab378
Dipesh Parmar <dipeshbeckham@gmail.com>
2025-07-16 20:30:51 +0530

    Added enum and common description method for each enum types

--- media/src/main.rs
+++ media/src/main.rs
@@ -1,3 +1,44 @@
+#[derive(Debug)]
+enum Media {
+  Book { title: String, author: String },
+  Movie { title: String, director: String },
+  AudioBook { title: String },
+}
+
+impl Media {
+  fn description(&self) -> String {
+    match self {
+      Media::AudioBook { title } => format!("Audio Book: {}", title),
+      Media::Movie { title, director } => format!("Movie: {} by {}", title, director),
+      Media::Book { title, author } => format!("Book: {} by {}", title, author),
+    }
+  }
+}
+
+fn print_media(media: Media) {
+  println!("Media: {:#?}", media);
+}
+
 fn main() {
-  println!("Hello, world!");
+  let audio_book = Media::AudioBook {
+    title: String::from("The Great Gatsby"),
+  };
+
+  let good_movie = Media::Movie {
+    title: String::from("The Good Movie"),
+    director: String::from("The Good Director"),
+  };
+
+  let bad_book = Media::Book {
+    title: String::from("The Bad Book"),
+    author: String::from("The Bad Author"),
+  };
+
+  println!("{}", audio_book.description());
+  println!("{}", good_movie.description());
+  println!("{}", bad_book.description());
+
+  print_media(audio_book);
+  print_media(good_movie);
+  print_media(bad_book);
 }
