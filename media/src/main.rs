d015a9aa152685a68eb0629cd360eca9590c5947
Dipesh Parmar <dipeshbeckham@gmail.com>
2025-07-16 22:04:49 +0530

    Added catalog

--- media/src/main.rs
+++ media/src/main.rs
@@ -3,6 +3,8 @@ enum Media {
   Book { title: String, author: String },
   Movie { title: String, director: String },
   AudioBook { title: String },
+  Podcast(u32),
+  Placeholder,
 }
 
 impl Media {
@@ -11,7 +13,24 @@ impl Media {
       Media::AudioBook { title } => format!("Audio Book: {}", title),
       Media::Movie { title, director } => format!("Movie: {} by {}", title, director),
       Media::Book { title, author } => format!("Book: {} by {}", title, author),
+      Media::Podcast(episode) => format!("Podcast: {}", episode),
+      Media::Placeholder => String::from("Placeholder"),
     }
+  }
+}
+
+#[derive(Debug)]
+struct Catalog {
+  items: Vec<Media>,
+}
+
+impl Catalog {
+  fn new() -> Self {
+    Catalog { items: vec![] }
+  }
+
+  fn add(&mut self, media: Media) {
+    self.items.push(media);
   }
 }
 
@@ -34,11 +53,23 @@ fn main() {
     author: String::from("The Bad Author"),
   };
 
-  println!("{}", audio_book.description());
-  println!("{}", good_movie.description());
-  println!("{}", bad_book.description());
+  let podcast = Media::Podcast(1);
+  let placeholder = Media::Placeholder;
 
-  print_media(audio_book);
-  print_media(good_movie);
-  print_media(bad_book);
+  // println!("{}", audio_book.description());
+  // println!("{}", good_movie.description());
+  // println!("{}", bad_book.description());
+
+  // print_media(audio_book);
+  // print_media(good_movie);
+  // print_media(bad_book);
+
+  let mut catalog = Catalog::new();
+  catalog.add(audio_book);
+  catalog.add(good_movie);
+  catalog.add(bad_book);
+  catalog.add(podcast);
+  catalog.add(placeholder);
+
+  println!("Catalog: {:#?}", catalog);
 }
